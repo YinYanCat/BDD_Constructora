151169f0328f52597e812366dd5a8b7770a58049
CondorPepinillo <marttapia2023@udec.cl>
2025-06-25 10:44:20 -0400

    Implementacion inicial de un login, para crear usuarios se hace desde consola

--- app/views.py
+++ app/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
+@login_required
 def home(request):
-    return render(request,'app/home.html')
+    return render(request, "app/home.html")
+
+def salir(request):
+    logout(request)
+    return redirect('/')
